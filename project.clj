758d7d3f76dfdbe1314c4745a9b432756dcf1d48
Ulf Ninow <mail@fnumatic.de>
2020-03-14 10:14:01 +0100

    feat: update deps

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject cljs-reframe-template "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]
-                 [reagent "0.8.1"]
-                 [re-frame "0.10.6"]
-                 [garden "1.3.6"]
-                 [metosin/reitit "0.2.9"]
-                 [metosin/reitit-schema "0.2.9"]
-                 [metosin/reitit-frontend "0.2.9"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [reagent "0.10.0"]
+                 [re-frame "0.12.0"]
+                 [garden "1.3.9"]
+                 [metosin/reitit "0.4.2"]
+                 [metosin/reitit-schema "0.4.2"]
+                 [metosin/reitit-frontend "0.4.2"]
 
-                 [ns-tracker "0.3.1"]
-                 [re-pressed "0.2.2" :exclusions [re-frame reagent]]]
+                 [ns-tracker "0.4.0"]
+                 [re-pressed "0.3.1" :exclusions [re-frame reagent]]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
@@ -22,11 +22,11 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.9.10"]
-                   [day8.re-frame/re-frame-10x "0.3.6"]
-                   [com.bhauman/figwheel-main "0.1.9"]
+   {:dependencies [[binaryage/devtools "1.0.0"]
+                   [day8.re-frame/re-frame-10x "0.6.0"]
+                   [com.bhauman/figwheel-main "0.2.3"]
                    [com.bhauman/rebel-readline-cljs "0.1.4"]
-                   [cider/piggieback "0.3.10"]]
+                   [cider/piggieback "0.4.2"]]
     :source-paths ["env/dev"]
     :resource-paths ["target"]
     :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
